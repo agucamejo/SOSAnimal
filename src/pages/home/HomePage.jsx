a79d31c38d4b6fbab7ed7e70fcc0e1f888702d03
Sol <solabigail00@hotmail.com>
2024-05-06 10:15:49 -0300

    add info & history

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,13 @@
+import Info from '../../components/home/Info'
+import Historia from '../../components/home/Historia'
+
 const Home = () => {
- return ( 
+  return (
     <div>
-        <h2>Home</h2>
-        <img className="container-subtitles" style={{height: "auto"}} src="https://cdn.memegenerator.es/imagenes/memes/full/33/36/33361906.jpg"></img>
-        <p style={{textAlign: "center"}}>El creador de meme no me dejaba poner letra más chica</p>
+      <Info />
+      <Historia />
     </div>
- );
+  )
 }
 
-export default Home;
+export default Home
