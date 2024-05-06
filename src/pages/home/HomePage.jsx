2cede6f5dcd607a7558dfc6df0fc4e52855a850c
Agustin Camejo <agustincamejo03@gmail.com>
2024-05-06 11:20:31 -0300

    Home complete

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -4,16 +4,18 @@ import Hero from '../../components/home/Hero';
 import Collaborations from '../../components/home/Collaborations';
 import Info from '../../components/home/Info';
 import Historia from '../../components/home/Historia';
+import Valores from '../../components/home/Valores';
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <Hero />
-        <Collaborations />
-        <Testimonials />
         <Info />
         <Historia />
+        <Collaborations />
+        <Valores />
+        <Testimonials />
     </div>
   )
 }
