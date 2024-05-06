4fdd38458e215270d0f0488823d02adf456ad499
andreJesusTorres <andre.torres@outlook.com.ar>
2024-05-06 15:14:52 +0200

    Valores

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,8 @@
+import Valores from '../../components/home/Valores';
 const Home = () => {
  return ( 
     <div>
-        <h2>Home</h2>
-        <img className="container-subtitles" style={{height: "auto"}} src="https://cdn.memegenerator.es/imagenes/memes/full/33/36/33361906.jpg"></img>
-        <p style={{textAlign: "center"}}>El creador de meme no me dejaba poner letra más chica</p>
+        <Valores />
     </div>
  );
 }
