93ac3f94605b536f025446e9baf6374cc7376254
Sol <solabigail00@hotmail.com>
2024-05-20 11:54:56 -0300

    colaborations page

--- src/pages/colaborations/ColabPage.jsx
+++ src/pages/colaborations/ColabPage.jsx
@@ -1,13 +1,26 @@
-import Navbar from "../../components/Navbar";
+import Navbar from '../../components/Navbar'
+import Socio from '../../components/collaborations/Socio'
+import Voluntario from '../../components/collaborations/Voluntario'
+import Donaciones from '../../components/collaborations/Donaciones'
+import Subtitles from '../../components/Subtitles'
 
 const Colaborations = () => {
-    return ( 
-       <div>
-           <Navbar />
-           <h2>Colaboraciones</h2>
-           <img className="container-subtitles" style={{height: "auto"}} src="https://cdn.memegenerator.es/imagenes/memes/full/33/36/33361926.jpg"></img>
-       </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <div className="contentColab">
+        <section className="colaborations">
+          <section className="contentTitlesColab">
+            <Subtitles />
+            <h2 className="titleColab">Cómo podés ayudar a los animales</h2>
+          </section>
+          <Socio />
+          <Voluntario />
+          <Donaciones />
+        </section>
+      </div>
+    </div>
+  )
 }
-   
-export default Colaborations;
+
+export default Colaborations
