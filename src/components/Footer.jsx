1b963c7ee373abd9776c35cb90c20e0f4754c545
andreJesusTorres <andre.torres@outlook.com.ar>
2024-05-27 16:05:59 +0200

    Cambios svg y colores

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,82 +1,82 @@
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    return (
-      <footer className="footer">
-        <div className="footer-inner">
-          <div className="footer-content">
-            <div className="contenedor-logo-redes">
-              <div className="logo">
-                <img
-                  src="images/logo-footer.png"
-                  alt="Logo SOS Animal Fray Bentos"
-                  className="logo-img"
-                />
-                <div className="logo-text">
-                  <h4>
-                    SOS Animal
-                    <br />
-                    Fray Bentos
-                  </h4>
-                </div>
+  return (
+    <footer className="footer">
+      <div className="footer-inner">
+        <div className="footer-content">
+          <div className="contenedor-logo-redes">
+            <div className="logo">
+              <img
+                src="images/logo.svg"
+                alt="Logo SOS Animal Fray Bentos"
+                className="logo-img"
+              />
+              <div className="logo-text">
+                <h4>
+                  SOS Animal
+                  <br />
+                  Fray Bentos
+                </h4>
               </div>
-              <div className="redes">
-                <div className="contact-info">
-                  <h2>Sigamos en contacto</h2>
-                  <div className="social-icons">
-                    <a
-                      href="mailto:animalsos2020@gmail.com"
-                      className="btn-contact"
-                    >
-                      <img
-                        src="images/mail-footer.png"
-                        alt="Correo electrónico"
-                      />
-                    </a>
-                    <a
-                      href="https://wa.me/59892336700"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="btn-contact"
-                    >
-                      <img src="images/whatsapp-footer.png" alt="Whatsapp" />
-                    </a>
-                    <a
-                      href="https://www.youtube.com/@sosanimal2438"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="btn-contact"
-                    >
-                      <img src="images/youtube-footer.png" alt="YouTube" />
-                    </a>
-                    <a
-                      href="https://www.facebook.com/SOSAnimalFrayBentos/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="btn-contact"
-                    >
-                      <img src="images/facebook-footer.png" alt="Facebook" />
-                    </a>
-                    <a
-                      href="https://www.instagram.com/sos_animal_2020/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="btn-contact"
-                    >
-                      <img src="images/instagram-footer.png" alt="Instagram" />
-                    </a>
-                  </div>
+            </div>
+            <div className="redes">
+              <div className="contact-info">
+                <h2>Sigamos en contacto</h2>
+                <div className="social-icons">
+                  <a
+                    href="mailto:animalsos2020@gmail.com"
+                    className="btn-contact"
+                  >
+                    <img
+                      src="images/mail.svg"
+                      alt="Correo electrónico"
+                    />
+                  </a>
+                  <a
+                    href="https://wa.me/59892336700"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn-contact"
+                  >
+                    <img src="images/whatsapp.svg" alt="Whatsapp" />
+                  </a>
+                  <a
+                    href="https://www.youtube.com/@sosanimal2438"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn-contact"
+                  >
+                    <img src="images/youtube.svg" alt="YouTube" />
+                  </a>
+                  <a
+                    href="https://www.facebook.com/SOSAnimalFrayBentos/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn-contact"
+                  >
+                    <img src="images/facebook.svg" alt="Facebook" />
+                  </a>
+                  <a
+                    href="https://www.instagram.com/sos_animal_2020/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn-contact"
+                  >
+                    <img src="images/instagram.svg" alt="Instagram" />
+                  </a>
                 </div>
               </div>
             </div>
           </div>
-          <div className="navegacion-general">
-            <div className="navegacion-CTA">
-              <p>Porque cada vida importa, luchamos por ellos</p>
-              <Link to="/adoptions" className="btn-primary">Adopciones</Link>
-              <Link to="/collaborations" className="btn-secondary">Donaciones</Link>
-            </div>
-            <div className="navegacion-enlaces">
+        </div>
+        <div className="navegacion-general">
+          <div className="navegacion-CTA">
+            <p>Porque cada vida importa, luchamos por ellos</p>
+            <Link to="/adoptions" className="btn-primary">Adopciones</Link>
+            <Link to="/collaborations" className="btn-secondary">Donaciones</Link>
+          </div>
+          <div className="navegacion-enlaces">
             <Link to="/">
               Inicio
               <svg
@@ -168,30 +168,30 @@ const Footer = () => {
               </svg>
             </Link>
           </div>
-            <div className="navegacion-ayuda">
-              <a>AYUDA</a>
-              <div className="enlaces-ayuda">
-                <Link to="/contact">Preguntas frecuentes</Link>
-                <Link to="/contact">Números de emergencia</Link>
-                <Link to="/contact">Charlas educativas</Link>
-              </div>
+          <div className="navegacion-ayuda">
+            <a className="no-hover">AYUDA</a>
+            <div className="enlaces-ayuda">
+              <Link to="/contact">Preguntas frecuentes</Link>
+              <Link to="/contact">Números de emergencia</Link>
+              <Link to="/contact">Charlas educativas</Link>
             </div>
           </div>
         </div>
-        <div className="ubicacion-derechos">
-          <div className="ubicacion">
-            <p>Fray Bentos, Río Negro, Uruguay</p>
-            <p>
-              <img
-                src="images/perro-mini.png"
-                alt="Perro mini"
-                className="perro-mini"
-              />
-            </p>
-          </div>
-          <div className="derechos">
-            <p>2023 @ SOS Animal. All rights reserved</p>
-            <div className="creditos">
+      </div>
+      <div className="ubicacion-derechos">
+        <div className="ubicacion">
+          <p>Fray Bentos, Río Negro, Uruguay</p>
+          <p>
+            <img
+              src="images/perrito.svg"
+              alt="Perro mini"
+              className="perro-mini"
+            />
+          </p>
+        </div>
+        <div className="derechos">
+          <p>2023 @ SOS Animal. All rights reserved</p>
+          <div className="creditos">
             <p>
               Mountiel font designed by{" "}
               <a
@@ -203,10 +203,10 @@ const Footer = () => {
               </a>
             </p>
           </div>
-          </div>
         </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;  
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
