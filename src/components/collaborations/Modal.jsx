f5ae4d832397605e71dd7250ff3807726b5071f9
Jorgelina Mariano <jorgelina.mariano@gmail.com>
2024-06-05 22:00:43 -0300

    details

--- src/components/collaborations/Modal.jsx
+++ src/components/collaborations/Modal.jsx
@@ -1,50 +1,50 @@
-import { Player } from '@lottiefiles/react-lottie-player'
-import { IoClose } from 'react-icons/io5'
-import { IoMdCopy } from 'react-icons/io'
-import { useState, useRef } from 'react'
+import { Player } from '@lottiefiles/react-lottie-player';
+import { IoClose } from 'react-icons/io5';
+import { IoMdCopy } from 'react-icons/io';
+import { useState, useRef } from 'react';
 import Toastify from 'toastify-js';
-import "toastify-js/src/toastify.css";
+import 'toastify-js/src/toastify.css';
 
 function Modal({ isOpen, onClose }) {
-  const [copied] = useState(false)
-  const [iconColor] = useState('#908b8b')
+  const [copied] = useState(false);
+  const [iconColor] = useState('#908b8b');
   const isDesktop = useRef(window.innerWidth > 1024);
 
   const handleImageClick = (event, message, number) => {
     const buttonRect = event.target.getBoundingClientRect();
 
-    navigator.clipboard.writeText(number)
+    navigator.clipboard
+      .writeText(number)
       .then(() => {
-        if (isDesktop.current){
-            Toastify({
-              text: message,
-              duration: 1000,
-              gravity: "top",
-              position: "left",
-              stopOnFocus: true,
-              offset: {
-                x: buttonRect.left + (buttonRect.width / 2) - 95, 
-                y: buttonRect.top - 80 
-              },
-              style: {
-                background: "linear-gradient(to right, #00b09b, #96c93d)",
-              },
-            }).showToast();
-        }
-        else{
-            Toastify({
-              text: message,
-              duration: 2000,
-              gravity: "top",
-              position: "right",
-              stopOnFocus: true,
-              offset: { 
-                y: 65
-              },
-              style: {
-                background: "linear-gradient(to right, #00b09b, #96c93d)",
-              },
-            }).showToast();
+        if (isDesktop.current) {
+          Toastify({
+            text: message,
+            duration: 1000,
+            gravity: 'top',
+            position: 'left',
+            stopOnFocus: true,
+            offset: {
+              x: buttonRect.left + buttonRect.width / 2 - 95,
+              y: buttonRect.top - 80,
+            },
+            style: {
+              background: 'linear-gradient(to right, #00b09b, #96c93d)',
+            },
+          }).showToast();
+        } else {
+          Toastify({
+            text: message,
+            duration: 2000,
+            gravity: 'top',
+            position: 'right',
+            stopOnFocus: true,
+            offset: {
+              y: 65,
+            },
+            style: {
+              background: 'linear-gradient(to right, #00b09b, #96c93d)',
+            },
+          }).showToast();
         }
       })
       .catch((error) => {
@@ -66,17 +66,20 @@ function Modal({ isOpen, onClose }) {
         />
 
         <section className="contentTextModal">
+          <p className="textModal" style={{ marginBottom: '15px' }}>
+            Datos para transferencias{' '}
+          </p>
           <p className="textModal">
             <span className="spanModal">Titular:</span> Fiorella Batista
           </p>
           <p className="textModal">
-            <span className="spanModal">Banco:</span> Midinero
+            <span className="spanModal">Banco:</span> RedPagos
           </p>
           <p className="textModal">
             <span className="spanModal">N° de cuenta:</span> 6145664
             <IoMdCopy
               className="copy"
-              onClick={(e) => handleImageClick(e, "Copiado", "6145664")}
+              onClick={(e) => handleImageClick(e, 'Copiado', '6145664')}
               style={{ color: iconColor }}
             />
           </p>
@@ -84,7 +87,7 @@ function Modal({ isOpen, onClose }) {
         </section>
       </section>
     </section>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -3,33 +3,35 @@ import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 
 const Error = () => {
-    const canonicalUrl = `https://sos-animal.vercel.app/*`;
-    const imgError = '/images/error404.png';
-    
-    return (
-       <div>
-            <Helmet>
-                <link rel="canonical" href={canonicalUrl} />
-                <title>No encontrado</title>
-            </Helmet>
-            <div className="error-container">
-              <div className="container-img">
-              <img src={imgError} alt="Error404" className="img" />
-                <div className="img-text">
-                  <h4>
-                    Parece que esta página se ha escapado corriendo <br /> como un
-                    cachorro travieso.
-                  </h4>
-                  <h4>No podemos encontrar lo que buscas.</h4>
-                  <p>
-                    Tal vez el enlace esté roto, la página haya sido eliminada o <br />{' '}
-                    simplemente hayas tecleado mal la dirección.
-                  </p>
-                </div>
-              </div>
-              <Link to="/"><ButtonPrimary text="Volver al inicio" className="button-inicio" /></Link>
-            </div>  
-       </div>
-    );
-  }
+  const canonicalUrl = `https://sos-animal.vercel.app/*`;
+  const imgError = '/images/error404.png';
+
+  return (
+    <div>
+      <Helmet>
+        <link rel="canonical" href={canonicalUrl} />
+        <title>No encontrado</title>
+      </Helmet>
+      <div className="error-container">
+        <div className="container-img">
+          <img src={imgError} alt="Error404" className="img" />
+          <div className="img-text">
+            <h4>
+              Parece que esta página se ha escapado corriendo <br /> como un
+              cachorro travieso.
+            </h4>
+            <h4>No podemos encontrar lo que buscas.</h4>
+            <p>
+              Tal vez el enlace esté roto, la página haya sido eliminada o{' '}
+              <br /> simplemente hayas tecleado mal la dirección.
+            </p>
+          </div>
+        </div>
+        <Link to="/">
+          <ButtonPrimary text="Volver al inicio" className="button-inicio" />
+        </Link>
+      </div>
+    </div>
+  );
+};
 export default Error;
